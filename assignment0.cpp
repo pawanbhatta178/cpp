21ee5d5161b0ea33c4f58a74bbe9c4cdd335624f
pawan <pawanbhatta178@gmail.com>
2021-02-24 19:25:43 -0500

    assignment0 polished

--- assignment0.cpp
+++ assignment0.cpp
@@ -4,23 +4,21 @@ using namespace std;
 
 // Base case for recursion
 template <class T>
-inline T sum(T n)
+T sum(T n)
 {
     return n;
 }
 
 // Recursive case
 template <class T, class... Args>
-inline T sum(T n, Args... args)
+T sum(T n, Args... args)
 {
     return n + sum(args...);
 }
 
 int main()
 {
-    string string1 = "Thuis ";
-    string string2 = " concatenation ";
-    string string3 = " works";
-
-    cout << "[Summing 4 numbers: 3, 44, 55, 66] -> " << sum(1, 2, 2, 3) << endl;
+    cout << "[Summing 3 numbers: 1, 2, 3] -> " << sum(1, 2, 3) << endl;
+    cout << "[Summing 2 numbers: 10,20] -> " << sum(10, 20) << endl;
+    cout << "[Summing 5 numbers: 1, 2, 3,4, 5] -> " << sum(1, 2, 3, 4, 5) << endl;
 }
