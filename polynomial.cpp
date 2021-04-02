b9ae16bedd6aecbab25244b1197e485f3ab2f535
pawan <pawanbhatta178@gmail.com>
2021-04-01 21:06:21 -0400

    consolidations of polynomials done

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <cstdlib>
-#include <cassert>
+#include <fstream>
+#include <string>
+#include <cstdarg>
+#include <sstream>
 using namespace std;
 
 class Term
@@ -19,22 +21,44 @@ public:
         next = nextTerm;
     }
 
-    ~Term()
-    {
-        //By overwriting default destructor
-    }
-
     friend ostream &operator<<(ostream &os, Term t)
     {
-
-        cout << t.coefficient << "x^" << t.exponent;
+        if (t.coefficient != 0)
+        {
+            if (t.exponent == 0)
+            {
+                cout << t.coefficient;
+            }
+            else if (t.exponent == 1)
+            {
+                cout << t.coefficient << "x";
+            }
+            else
+            {
+                cout << t.coefficient << "x^" << t.exponent;
+            }
+        }
         return os;
     }
 
-    friend ostream &operator<<(ostream &os, Term &t)
+    friend ostream &operator<<(ostream &os, Term *t)
     {
 
-        cout << t.coefficient << "x^" << t.exponent;
+        if (t->coefficient != 0)
+        {
+            if (t->exponent == 0)
+            {
+                cout << t->coefficient;
+            }
+            else if (t->exponent == 1)
+            {
+                cout << t->coefficient << "x";
+            }
+            else
+            {
+                cout << t->coefficient << "x^" << t->exponent;
+            }
+        }
         return os;
     }
 };
@@ -42,12 +66,13 @@ public:
 class Polynomial
 {
 private:
-    Term *head;
+    Term *head, *tail;
 
 public:
     Polynomial()
     {
         head = NULL;
+        tail = NULL;
     }
 
     void insertTerm(int coef, int pow)
@@ -58,30 +83,104 @@ public:
             return;
         }
 
-        Term t(coef, pow, NULL);
+        Term *t = new Term(coef, pow, NULL);
 
         if (head == NULL)
         {
-            head = &t;
+            head = t;
+            tail = t;
         }
         else
         {
-            Term *current = head;
-            while (current != NULL)
+            tail->next = t;
+            tail = tail->next;
+        }
+    }
+
+    void consolidate()
+    {
+        insertionSort(&head);
+        Term *current = head;
+        while (current != NULL)
+        {
+            if (current->next != NULL && current->exponent == current->next->exponent)
+            {
+                current->coefficient = current->coefficient + current->next->coefficient;
+                Term *temp = current->next->next;
+                current->next = temp;
+                continue;
+            }
+            current = current->next;
+        }
+    }
+
+    void insertionSort(Term **head_ref)
+    {
+        // Initialize sorted linked list
+        Term *sorted = NULL;
+
+        // Traverse the given linked list and insert every
+        // node to sorted
+        Term *current = *head_ref;
+        while (current != NULL)
+        {
+            // Store next for next iteration
+            Term *next = current->next;
+
+            // insert current in sorted linked list
+            sortedInsert(&sorted, current);
+
+            // Update current
+            current = next;
+        }
+
+        // Update head_ref to point to sorted linked list
+        *head_ref = sorted;
+    }
+
+    void sortedInsert(Term **head_ref, Term *new_node)
+    {
+        Term *current;
+        /* Special case for the head end */
+        if (*head_ref == NULL || (*head_ref)->exponent <= new_node->exponent)
+        {
+            new_node->next = *head_ref;
+            *head_ref = new_node;
+        }
+        else
+        {
+            /* Locate the node before the point of insertion */
+            current = *head_ref;
+            while (current->next != NULL &&
+                   current->next->exponent > new_node->exponent)
             {
                 current = current->next;
             }
-            current = &t;
+            new_node->next = current->next;
+            current->next = new_node;
         }
-        // cout << t << endl;
     }
 
     void printPolynomial()
     {
-
-        cout << head->exponent << endl;
-        cout << head->next->exponent << endl;
-        cout << head->next->next->exponent << endl;
+        Term *t = head;
+        while (t != NULL)
+        {
+            if (t == head)
+            {
+                cout << t;
+            }
+            else
+            {
+                if (t->coefficient > 0)
+                {
+                    cout << "+";
+                }
+                cout << t;
+            }
+            t = t->next;
+        }
+        cout << endl;
     }
 
     // friend ostream &operator<<(ostream &os, Polynomial p)
@@ -119,9 +218,28 @@ public:
 
 int main()
 {
-    Polynomial p;
-    p.insertTerm(2, 3);
-    p.insertTerm(9, 9);
-    p.insertTerm(9, 4);
-    p.printPolynomial();
+    //READ
+    string inputName = "input.txt";
+    ifstream input;
+    input.open(inputName);
+
+    if (input.is_open())
+    {
+        for (string line; getline(input, line);)
+        {
+            istringstream ss(line);
+            int coef, exp;
+            Polynomial p;
+            while (ss >> coef && ss >> exp)
+            {
+                p.insertTerm(coef, exp);
+            }
+            p.consolidate();
+            p.printPolynomial();
+        }
+    }
+    else
+    {
+        cout << "ERROR: The input file with following name does not exists or there was problem reading it: " << inputName << endl;
+    }
 }
